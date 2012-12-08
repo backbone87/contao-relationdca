5d89ed9e355f48aacdc44a48e5d13fc299b3c3aa
Oliver Hoff <oliver@hofff.com>
2012-12-08 14:06:56 +0100

    contao 3 compat

---
+++ TL_ROOT/system/modules/backboneit_relationdca/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package Backboneit_relationdca
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'RelationDCA' => 'system/modules/backboneit_relationdca/RelationDCA.php',
+));
